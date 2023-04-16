aef289671b3955043649de577a66f55fcfb7c7f5
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2023-04-16 10:53:38 +0000

    gulp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,3 +36,113 @@ function App() {
       else {
         return response.json();
       }
+    })
+    .then((data) => {
+      if (data.message === "Pong!") {
+        ValidateURL();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  function ValidateURL() {
+    setProgress("Validating URL...");
+
+    // Get input
+    let text = urlInputRef.current.value;
+    if (text === "") {
+      text = "None"
+    }
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ url: text })
+    };
+    link = text;
+    // Create get request
+    fetch(API_ENDPOINT + "/validate", requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.Valid === 1) {
+        // Cleanup
+        showTranscriptLoader(true);
+        GenerateTranscript();
+      }
+      else {
+        setProgress("Invalid URL.");
+      }
+    });
+  }
+
+  function GenerateTranscript() {
+    setProgress("Fetching Transcript...");
+    fetch(API_ENDPOINT + "/gentranscript")
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.Completed === 1) {
+        setUrl(link)
+        showChatbox(true);
+        Scroll.animateScroll.scrollTo(420);
+        setProgress("");
+        showTranscriptLoader(false);
+      }
+      else if (data.Completed === 404) {
+        setProgress("Requested video has no transcript. Please try a new video.")
+        showTranscriptLoader(false);
+      }
+      else if (data.Completed === 403) {
+        setProgress("Requested article has no text. Please try a new article.")
+        showTranscriptLoader(false);
+      }
+    });
+  }
+
+  function HandleAnalyzeClicked(e) {
+    CheckServerStatus();
+  }
+
+  function checkURLKeyDown(e) {
+    if (e.key === 'Enter') {
+      HandleAnalyzeClicked();
+    }
+  }
+
+	return (
+		<>
+      <Header/>
+      <div className="title">
+        <h1>Re</h1>
+        <h1 className="titleAI">AI</h1>
+        <h1>nvent</h1>
+      </div>
+      <h3>
+        Enter Youtube URL below:
+      </h3>
+      <div className="urlInput">
+        <GrYoutube color = "red" size = "30px" style = {{
+          marginBottom: "-7px"
+        }}/>
+        <input id="urlTextInput" name="linkInput" type="text" ref={urlInputRef} onKeyDown={checkURLKeyDown}/>
+      </div>
+      <br/>
+      <input type="submit" id="analyzeBtn" value="Analyze" onClick={HandleAnalyzeClicked}/>
+      <br/>
+      <h3>{progress}</h3>
+      {
+        transcriptLoader ? (<div className="progressLoader"><AtomLoader {...loaderProps}/></div>) : (<div/>)
+      }
+      {
+        chatbox ? <div>
+                    <div>
+                      <ReactPlayer ref={playerRef} controls={true} url={url} id="videoplayer" playing={isPlaying}/>
+                    </div>
+                    <Chatbox playerRef={playerRef} endpoint={API_ENDPOINT} setPlaying={setPlaying}/>
+                  </div> : <></>
+      }
+		</>
+	);
+}
+
+export default App;
