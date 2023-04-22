0f1d703ba3e4a857cb58952091371943192f639c
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2023-04-22 17:12:55 +0000

    melhorias

--- frontend/src/Chatbox.js
+++ frontend/src/Chatbox.js
@@ -1,3 +1,10 @@
 import React, { useRef, useState } from "react";
 import QandA from './QandA.js';
-import { FiSend } from 'react-i
+import { FiSend } from 'react-icons/fi';
+import "./Chatbox.css";
+
+function Chatbox(args) {
+    const questionRef = useRef();
+    const [chat, setChat] = useState([]);
+    function setQuestion(e) {
+        questionRef.current.value = e.currentTarge
