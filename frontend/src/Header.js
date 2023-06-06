72dca0a0782856264aa1d297785d309e4940031c
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2023-06-06 08:38:55 +0000

    Progress...

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,1 +1,10 @@
-import Re
+import React from "react";
+import './Header.css';
+import { BsGithub } from 'react-icons/bs';
+
+function Header() {
+	return (
+		<div className = "header">
+            <div className = "logo">
+                <a href="/">
+                   
